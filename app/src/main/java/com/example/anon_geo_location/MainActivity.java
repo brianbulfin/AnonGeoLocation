fd22d7188c7c02828409b75fda5b49d603919887
brianbulfin <+$8],DNt6*4sc.=G>
2021-01-23 12:50:48 +0000

    Created initial layout for user register screen, Added progress bar to login and register screen, Added onClickListener to MainActivity and implemented switch statement for multiple component clicks

--- app/src/main/java/com/example/anon_geo_location/MainActivity.java
+++ app/src/main/java/com/example/anon_geo_location/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.anon_geo_location;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        register = (TextView) findViewById(R.id.loginRegisterLink);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.loginRegisterLink:
+                startActivity(new Intent(this, RegisterUserActivity.class));
+                break;
+        }
     }
 }
